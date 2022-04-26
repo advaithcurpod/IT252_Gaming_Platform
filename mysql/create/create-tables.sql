f28ef70c95c3dc280a2a2874f4a678e42503aa64
famousfive1 <tejaswihegde1@gmail.com>
2022-04-26 13:17:16 +0530

    finish all tables

--- mysql/create/create-tables.sql
+++ mysql/create/create-tables.sql
@@ -54,3 +54,74 @@ create table Game (
     primary key (game_id),
     constraint valid_price check(game_price>=0)
 );
+
+create table Member_of (
+    team_id int,
+    dev_id int,
+
+    foreign key (team_id) references DevTeam(team_id),
+    foreign key (dev_id) references Developer(dev_id)
+);
+
+create table Review (
+    user_id int,
+    game_id int,
+    stars float,
+    review_text text,
+    date_time datetime default CURRENT_TIMESTAMP,
+
+    foreign key (user_id) references User (user_id),
+    foreign key (game_id) references Game (game_id)
+);
+
+create table Plays (
+    user_id int,
+    game_id int,
+    score int,
+    play_time time,
+
+    foreign key (user_id) references User (user_id),
+    foreign key (game_id) references Game (game_id)
+);
+
+create table Transactions (
+    tx_id int,
+    payer int,
+    id int,
+    id_type ENUM('USER', 'GAME', 'ITEM'),
+    tx_amt float,
+    tx_time datetime default CURRENT_TIMESTAMP,
+
+    primary key (tx_id),
+    check (tx_amt > 0)
+);
+
+create table Item (
+    item_id int,
+    item_name varchar(50),
+    item_type ENUM('PLATFORM', 'GAME'),
+
+    primary key (item_id)
+);
+
+create table PlatformItem (
+    item_id int,
+    item_cost float,
+    item_creator int,
+
+    foreign key (item_id) references Item (item_id),
+    foreign key (item_creator) references User (user_id),
+    check (item_cost >= 0)
+);
+
+create table GameItem (
+    item_id int,
+    quantity int,
+    item_worth float,
+    game_id int,
+
+    foreign key (item_id) references Item (item_id),
+    foreign key (game_id) references Game (game_id),
+    check (quantity >= 0),
+    check (item_worth >= 0)
+);
