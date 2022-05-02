51ce88d42ff4651d0dd2c1db5ff67dad4aa7d007
Ranjana Kambhammettu <ranjanak207@gmail.com>
2022-05-02 15:13:22 +0530

    Added details in insert

--- mysql/insert/insert_values.sql
+++ mysql/insert/insert_values.sql
@@ -7,7 +7,7 @@ INSERT INTO User VALUES (65423, 'Tejaswi', 'tejaswi456@#');
 INSERT INTO User VALUES (82423, 'Hegde', 'hegde433@#');
 INSERT INTO User VALUES (86543, 'Thomas', 'thomas832@#');
 INSERT INTO User VALUES (23486, 'Mary', 'mary324@#');
-INSERT INTO User VALUES (4394343943, 'Prasad', 'prasad943@#');
+INSERT INTO User VALUES (43943, 'Prasad', 'prasad943@#');
 INSERT INTO User VALUES (23435, 'Ramya', 'ramya451@#');
 INSERT INTO User VALUES (38912, 'George', 'george847@#');
 INSERT INTO User VALUES (79456, 'Tina', 'tina238@#');
@@ -35,19 +35,19 @@ INSERT INTO Developer(dev_id, dev_role, cash_earned) VALUES (23435, 'Lead Progra
 INSERT INTO Developer(dev_id, dev_role, cash_earned) VALUES (38912, 'Lead Artist', 230);
 INSERT INTO Developer(dev_id, dev_role, cash_earned) VALUES (93451, 'Game Tester', 9020);
 
-INSERT INTO Chats VALUES(23424, 79456, '');
-INSERT INTO Chats VALUES(12412, 43943, '');
-INSERT INTO Chats VALUES(86543, 79456, '');
-INSERT INTO Chats VALUES(93451, 23486, '');
-INSERT INTO Chats VALUES(32432, 86543, '');
+INSERT INTO Chats VALUES(23424, 79456, 'Hello Tina, I am Swetha!');
+INSERT INTO Chats VALUES(12412, 43943, 'Yo Prasad, what is your best score today in PUBG?');
+INSERT INTO Chats VALUES(86543, 79456, 'Have you started playing League of Legends?');
+INSERT INTO Chats VALUES(93451, 23486, 'Mary, wanna play Minecraft now?');
+INSERT INTO Chats VALUES(32432, 86543, 'Hey Thomas, coming to battle it out on Fortnite?');
 
-INSERT INTO DevTeam VALUES(1, '');
-INSERT INTO DevTeam VALUES(2, '');
+INSERT INTO DevTeam VALUES(1, 'DarkPlayerGames');
+INSERT INTO DevTeam VALUES(2, 'SolarPieGames');
 
-INSERT INTO Game VALUES(1, 'PUBG', 50, 'PUBG is a player versus player shooter game in which up to one hundred players fight in a battle royale, a type of large-scale last man standing deathmatch where players fight to remain the last alive.','Multiplayer, Battle royale', 1);
-INSERT INTO Game VALUES(2, 'Fortnite', 50, '', '', 2);
-INSERT INTO Game VALUES(3, 'League of Legends', 50, '', '', 2);
-INSERT INTO Game VALUES(4, 'Minecraft', 50, '', '', 1);
+INSERT INTO Game VALUES(1, 'PUBG', 50, 'PUBG is a player versus player shooter game in which up to one hundred players fight in a battle royale, a type of large-scale last man standing deathmatch where players fight to remain the last alive.','Multiplayer, Battle Royale', 1);
+INSERT INTO Game VALUES(2, 'Fortnite', 50, 'Fortnite is a free-to-play Battle Royale game with numerous game modes for every type of game player. Watch a concert, build an island or fight.', 'Multiplayer, Battle Royale', 2);
+INSERT INTO Game VALUES(3, 'League of Legends', 50, 'League of Legends is a team-based game with over 140 champions to make epic plays with.', 'Multiplayer, Online Battle Arena, Action Role-Playing', 2);
+INSERT INTO Game VALUES(4, 'Minecraft', 50, 'Explore infinite worlds and build everything from the simplest of homes to the grandest of castles.', 'Sandbox, Survival, Action, Adventure', 1);
 
 
 INSERT INTO Member_of VALUES(1, 32432);
@@ -63,7 +63,7 @@ INSERT INTO Review(user_id, game_id, stars, review_text) VALUES (79456, 2, 4, "A
 INSERT INTO Review(user_id, game_id, stars, review_text) VALUES (23486, 1, 1, "Game takes ages to load :(" );
 INSERT INTO Review(user_id, game_id, stars, review_text) VALUES (12412, 2, 5, "Love the game! The graphics of this game are excellent" );
 INSERT INTO Review(user_id, game_id, stars, review_text) VALUES (79456, 3, 5, "I play this game daily...so much fun" );
-INSERT INTO Review(user_id, game_id, stars, review_text) VALUES (86543, 4, 2, "Visuls could be better" );
+INSERT INTO Review(user_id, game_id, stars, review_text) VALUES (86543, 4, 2, "Visuals could be better" );
 
 INSERT INTO Item VALUES(1, 'Gems', 'PLATFORM');
 INSERT INTO Item VALUES(2, 'Avatar1', 'PLATFORM');
