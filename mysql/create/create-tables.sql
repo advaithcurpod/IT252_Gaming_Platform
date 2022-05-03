5638ac1922bc980c4e5d4b2ba1b24dd0565ac156
Advaith <advaithcurpod@gmail.com>
2022-05-03 19:00:27 +0530

    merge conflict resolved

--- mysql/create/create-tables.sql
+++ mysql/create/create-tables.sql
@@ -1,5 +1,4 @@
 use gaming_platform;
-
 create table User (
     user_id int primary key auto_increment,
     user_name varchar(50) not null unique,
@@ -8,24 +7,26 @@ create table User (
 create table Player (
     player_id int primary key,
     coins int default 0,
-    account_level varchar(50) default 'noob',
-    
+    account_level enum('noob', 'beginner', 'pro', 'legend') default 'noob',
     foreign key (player_id) references User(user_id),
-    check(coins >= 0)
+    check(coins >= 0),
+    check(player_id >= 0)
 );
 create table Developer (
     dev_id int primary key,
     dev_role varchar(50) not null,
     cash_earned int default 0,
     skill_rating float default 0.0,
-    
-    foreign key (dev_id) references Player(player_id)
+    -- out of 5
+    skill_rating float default 0.0,
+    foreign key (dev_id) references Player(player_id),
+    check(cash_earned >= 0)
 );
 create table Chats (
     player1_id int not null,
     player2_id int not null,
     message varchar(10000),
-    
+    primary key (player1_id, player2_id),
     foreign key (player1_id) references Player (player_id),
     foreign key (player2_id) references Player (player_id),
     check(player1_id <> player2_id)
@@ -42,14 +43,12 @@ create table Game (
     description text not null,
     tags text not null,
     team_id int,
-    
     foreign key (team_id) references DevTeam(team_id),
     check(game_price >= 0)
 );
 create table Member_of (
     team_id int,
     dev_id int,
-    
     primary key(team_id, dev_id),
     foreign key (team_id) references DevTeam(team_id),
     foreign key (dev_id) references Developer(dev_id)
@@ -60,44 +59,39 @@ create table Review (
     stars float,
     review_text text,
     date_time datetime default CURRENT_TIMESTAMP,
-    
     primary key (user_id, game_id),
-
     foreign key (user_id) references User (user_id),
     foreign key (game_id) references Game (game_id)
 );
 create table Plays (
-    user_id int,
-    game_id int,
-    score int,
+    user_id int not null,
+    game_id int not null,
+    score int default 0,
     play_time time,
-
+    primary key (user_id, game_id),
     foreign key (user_id) references User (user_id),
-    foreign key (game_id) references Game (game_id)
+    foreign key (game_id) references Game (game_id),
+    check(score >= 0)
 );
 create table Transactions (
-    tx_id int,
-    payer int,
-    id int,
-    id_type ENUM('USER', 'GAME', 'ITEM'),
-    tx_amt float,
+    tx_id int primary key,
+    payer int not null,
+    id int not null,
+    id_type ENUM('USER', 'GAME', 'ITEM') not null,
+    tx_amt float not null,
     tx_time datetime default CURRENT_TIMESTAMP,
-
-    primary key (tx_id),
+    primary key(payer, id_type, id, tx_time),
     check (tx_amt > 0)
 );
 create table Item (
-    item_id int,
+    item_id int primary key,
     item_name varchar(50),
-    item_type ENUM('PLATFORM', 'GAME'),
-
-    primary key (item_id)
+    item_type ENUM('PLATFORM', 'GAME')
 );
 create table PlatformItem (
     item_id int,
     item_cost float,
     item_creator int,
-
     foreign key (item_id) references Item (item_id),
     foreign key (item_creator) references User (user_id),
     check (item_cost >= 0)
@@ -107,7 +101,6 @@ create table GameItem (
     quantity int,
     item_worth float,
     game_id int,
-
     foreign key (item_id) references Item (item_id),
     foreign key (game_id) references Game (game_id),
     check (quantity >= 0),
--- mysql/triggers.sql
+++ mysql/triggers.sql
@@ -1,72 +1,117 @@
 -- update the account level when user score in games increases
 drop trigger if exists insert_account_level;
-delimiter $$
-create trigger insert_account_level
-after insert on Plays
-for each row
-begin
-	-- select avg(score) into @avg_score from Plays;
-	if(score between 0 and 100) then
-    update Player set account_level = 'noob';
-	elseif(score between 100 and 1000) then
-	update Player set account_level = 'beginner';
-	elseif(score between 1000 and 2000) then
-	update Player set account_level = 'average';
-	elseif(score between 2000 and 5000) then
-	update Player set account_level = 'pro';
-	elseif(score > 5000) then
-	update Player set account_level = 'legend';
-	end if;
-end $$
-delimiter ;
-
-
-
+delimiter $$ create trigger insert_account_level
+after
+insert on Plays for each row begin -- select avg(score) into @avg_score from Plays;
+	if(
+		score between 0 and 100
+	) then
+update Player
+set account_level = 'noob';
+elseif(
+	score between 100 and 1000
+) then
+update Player
+set account_level = 'beginner';
+elseif(
+	score between 1000 and 2000
+) then
+update Player
+set account_level = 'average';
+elseif(
+	score between 2000 and 5000
+) then
+update Player
+set account_level = 'pro';
+elseif(score > 5000) then
+update Player
+set account_level = 'legend';
+end if;
+end $$ delimiter;
 -- update the skill_rating of all developers when review is posted
 -- gives an error "result consisted of more than 1 row"
 drop trigger if exists update_developer_skill_on_insert;
-delimiter $$
-create trigger update_developer_skill_on_insert
-after insert on Review
-for each row
-begin
-select avg(stars) into @avg_stars from Review group by game_id;
-update Developer set skill_rating = (select avg(stars) from Review where new.game_id = (select game_id from Review)) where dev_id in (select dev_id from Member_of where team_id in (select team_id from Game where game_id = new.game_id));
-end $$
-delimiter ;
-
+delimiter $$ create trigger update_developer_skill_on_insert
+after
+insert on Review for each row begin
+select avg(stars) into @avg_stars
+from Review
+group by game_id;
+update Developer
+set skill_rating = (
+		select avg(stars)
+		from Review
+		where new.game_id = (
+				select game_id
+				from Review
+			)
+	)
+where dev_id in (
+		select dev_id
+		from Member_of
+		where team_id in (
+				select team_id
+				from Game
+				where game_id = new.game_id
+			)
+	);
+end $$ delimiter;
 -- update the skill_rating of all developers when review is updated
 drop trigger if exists update_developer_skill_on_update;
-delimiter $$
-create trigger update_developer_skill_on_update
-after update on Review
-for each row
-begin
-select avg(stars) into @avg_stars from Review having game_id = new.game_id;
-update Developer set skill_rating = avg_stars where dev_id in (select dev_id from Member_of where team_id in (select team_id from Game where game_id = new.game_id));
-end $$
-delimiter ;
-
+delimiter $$ create trigger update_developer_skill_on_update
+after
+update on Review for each row begin
+select avg(stars) into @avg_stars
+from Review
+having game_id = new.game_id;
+update Developer
+set skill_rating = avg_stars
+where dev_id in (
+		select dev_id
+		from Member_of
+		where team_id in (
+				select team_id
+				from Game
+				where game_id = new.game_id
+			)
+	);
+end $$ delimiter;
 -- listen to transactions and credit and debit from the appropriate accounts
 drop trigger if exists credit_app_entity;
-delimiter $$
-create trigger credit_app_entity
-after insert on Transactions
-for each row
-begin
-select count(*) into @n from Member_of where team_id in (select team_id from Game where game_id = id);
-if(id_type = 'GAME' or id_type = 'ITEM') then
-update Developer set cash_earned = (new.tx_amt)/n where dev_id in (select dev_id from Member_of where team_id in (select team_id from Game where game_id = id));
+delimiter $$ create trigger credit_app_entity
+after
+insert on Transactions for each row begin
+select count(*) into @n
+from Member_of
+where team_id in (
+		select team_id
+		from Game
+		where game_id = id
+	);
+if(
+	id_type = 'GAME'
+	or id_type = 'ITEM'
+) then
+update Developer
+set cash_earned = (new.tx_amt) / n
+where dev_id in (
+		select dev_id
+		from Member_of
+		where team_id in (
+				select team_id
+				from Game
+				where game_id = id
+			)
+	);
 elseif(id_type = 'USER') then
-update Player set coins = coins + new.tx_amt where player_id = id;
+update Player
+set coins = coins + new.tx_amt
+where player_id = id;
 end if;
-update Player set coins = coins - new.tx_amt where player_id = payer;
-end $$
-delimiter ;
-
-
-
-
-
+update Player
+set coins = coins - new.tx_amt
+where player_id = payer;
+end $$ delimiter;
 -- trigger testing
-select * from Developer;
+select *
+from Developer;
