ec1636be18fb9a2aba5d14416e4cbe59715ae3b2
Swetha Thomas <swethat1410@gmail.com>
2022-05-03 09:27:57 +0530

    Completed functions

--- mysql/functions.sql
+++ mysql/functions.sql
@@ -1,41 +1,45 @@
 use gaming_platform;
-DELIMITER $$ CREATE FUNCTION find_avgRating (gid INT) RETURNS FLOAT DETERMINISTIC BEGIN
-DECLARE avg_rating FLOAT;
-SET avg_rating = (
-		SELECT avg(stars)
-		FROM Review
-		WHERE game_id = gid
-	);
-RETURN avg_rating;
-END $$ DELIMITER;
-SELECT find_avgRating(2);
-DROP FUNCTION if exists find_amt_spent;
-DELIMITER $$ CREATE FUNCTION find_amt_spent(uid INT) RETURNS FLOAT DETERMINISTIC BEGIN
-DECLARE total_spent FLOAT;
-SET total_spent = (
-		SELECT sum(tx_amt)
-		FROM Transactions
-		WHERE payer = uid
-	);
-RETURN total_spent;
-END $$ DELIMITER;
-SELECT find_amt_spent(79456);
-SELECT *
-FROM Transactions;
 
-
-DROP FUNCTION if exists best_action_game;
+DROP FUNCTION if exists find_avgRating;
 DELIMITER $$
-CREATE FUNCTION best_action_game(uid INT) RETURNS FLOAT DETERMINISTIC BEGIN
-DECLARE total_spent FLOAT;
-SET total_spent = (
-		SELECT sum(tx_amt)
-		FROM Transactions
-		WHERE payer = uid
-	);
-RETURN total_spent;
+CREATE FUNCTION find_avgRating (gid INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+	DECLARE avg_rating FLOAT;
+	SET avg_rating = (SELECT avg(stars) FROM Review WHERE game_id=gid);
+	RETURN avg_rating;
 END $$
 DELIMITER ;
+SELECT find_avgRating(2);
+
+
+
+DROP FUNCTION if exists find_amt_spent;
+DELIMITER $$
+CREATE FUNCTION find_amt_spent(uid INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+	DECLARE total_spent FLOAT;
+	SET total_spent = (SELECT sum(tx_amt) FROM Transactions WHERE payer=uid);
+	RETURN total_spent;
+END $$
+DELIMITER ;
+SELECT find_amt_spent(79456);
+
+
+DROP FUNCTION if exists user_best_game;
+DELIMITER $$
+CREATE FUNCTION user_best_game(uid INT) RETURNS varchar(50) DETERMINISTIC BEGIN
+	DECLARE best_game varchar(50);
+	SET best_game = (
+					SELECT game_name 
+                    FROM Plays p, Game g 
+                    WHERE user_id=uid and p.game_id=g.game_id and 
+                    p.score=(SELECT MAX(score) FROM Plays)
+                    );
+	RETURN best_game;
+END $$
+DELIMITER ;
+SELECT user_best_game(38912);
+
 
 -- number of transations per day
 DROP FUNCTION if exists txs_per_day;
@@ -53,18 +57,6 @@ DELIMITER ;
 
 select txs_per_day("2022-05-03");
 
--- DROP FUNCTION if exists best_action_game;
--- DELIMITER $$
--- CREATE FUNCTION best_action_game(uid INT) RETURNS FLOAT DETERMINISTIC BEGIN
--- DECLARE total_spent FLOAT;
--- SET total_spent = (
--- 		SELECT sum(tx_amt)
--- 		FROM Transactions
--- 		WHERE payer = uid
--- 	);
--- RETURN total_spent;
--- END $$
--- DELIMITER ;
 
 -- number of positive reviews for a particular game
 DROP FUNCTION if exists positive_reviews;
